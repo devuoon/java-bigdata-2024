865befcef3e746eb5c11503f8792b07a348bf7b9
yoonji <dbswl341@naver.com>
2024-02-21 14:29:37 +0900

    0221

--- day01/p03_string.py
+++ day01/p03_string.py
@@ -9,3 +9,23 @@ print(b)
 
 c = "Hello, 'Ashley'"
 print(c)
+
+#탈출문자 \n, \t외 나머지는 파이썬에서 잘 사용되지 않음
+
+#문장을 여러줄 쓰고 싶으면
+d = 'Hello~ \n'
+'I\'m yoonji'
+'Nice to meet you'
+
+#여러줄 문장을 쓸때는 '''. """
+d = '''Hello~
+I'm yoonji.
+Nice to meet you too'''
+print(d)
+
+# 문자열 연산 +, *
+before = 'I wanna go to '
+after = 'Boracai'
+
+print(before + after) #I wanna go to Boracai (문자열을 합쳐서 하나의 문자열)
+print(after * 3) #BoracaiBoracaiBoracai (문자열을 몇 번 반복)
