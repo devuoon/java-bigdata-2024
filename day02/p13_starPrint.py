9892fa4c29d111de4d3155eba2c004374131472d
yoonji <dbswl341@naver.com>
2024-02-22 15:31:00 +0900

    0222

---
+++ day02/p13_starPrint.py
@@ -0,0 +1,17 @@
+# file : p13_starPrint.py
+# desc : 별찍기
+
+print('예제그림')
+for i in range(1, 6):
+  print('*' * i)
+
+# 2중 for 문으로 만들기
+print('\n')
+print('별찍기 ------->')
+for x in range(1,6):
+  for y in range(x, 6):
+    print('*', end='')
+    #print(' ', end='')
+    #print('*', end='')
+  print()
+  
