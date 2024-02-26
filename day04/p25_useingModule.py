1b01cba54769c1f73f65eb73ea2c7c82b6009ca2
yoonji <dbswl341@naver.com>
2024-02-26 10:34:17 +0900

    0226

--- day04/p25_useingModule.py
+++ day04/p25_useingModule.py
@@ -4,7 +4,11 @@
 import calc # calc.py를 사용하겠다
 from calc import mul # mul() 함수명만 쓰면 됨
 
-if __name__ == '__main__':
+from Math import Math # from Math는 모듈(파일이름) import Math는 클래스 이름
+
+from day03.p22_carClass import Car
+
+if __name__ == '__main__': ## java void main()과 동일
   result = mul(10,7)
   print(result)
 
---
+++ day04/p26_exceptionHandle.py
@@ -0,0 +1,14 @@
+# file : p26_exceptionHandle.py
+# desc : 예외처리
+# 오류(Error) 코드상 빨간색(노란색) 밑줄이 그어지는 것
+# 
+
+f = open('./sample.txt', mode='r', encoding='utf-8')
+#blah blah blah
+f.close()
+
+#print(5 / 0)
+
+#a, b = 5, 3
+#if a < b:
+#  print('True')
