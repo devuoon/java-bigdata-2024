fcef21455a5a5f9dcb4bdc072cf3023915bb2365
yoonji <dbswl341@naver.com>
2024-02-26 12:07:23 +0900

    0226

--- day04/p26_exceptionHandle.py
+++ day04/p26_exceptionHandle.py
@@ -4,13 +4,27 @@
 # 예외(Exception) : 프로그램 실행 중에 생기는 오류(비정상적 종료)
 
 # 1. 파일 읽기 - 예외발생(2번으로 넘어가기 불가능)
-f = open('./sample.txt', mode='r', encoding='utf-8')
-#blah blah blah
-res = f.readlin()
-f.close()
+try:
+  f = open('./sample.txt', mode='r', encoding='utf-8')
+  #blah blah blah
+  res = f.readlin
+  print(res)
+except:
+  print('파일오픈 예외발생!!')
+else:
+  f.close() #오류가 없는 경우만 수행
 
+#finally:
+#    try: # try-catch문을 이중으로 사용하면 성능에 좋지 않다.
+#      f.close()
+#    except NameError as e:
+#      print('파일 오픈 실패')
+    
 # 2. 계산결과
-#print(5 / 0)
+try:
+  print(5 / 0)
+except ZeroDivisionError as e:
+  print('나누기 예외발생!')
 
 #a, b = 5, 3
 #if a < b:
