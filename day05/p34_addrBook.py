49574ddb65185a5b2a0144595a11245d5873e8e2
yoonji <dbswl341@naver.com>
2024-02-27 11:42:04 +0900

    0227

--- day05/p34_addrBook.py
+++ day05/p34_addrBook.py
@@ -1,6 +1,8 @@
 # file : p34_addrBook.py
 # desc : 콘솔 주소록 프로그램
 
+import os # OS에서 필요한 모듈
+
 class Contact: # 주소록 클래스
   # 생성자
   __name = ''
@@ -28,7 +30,15 @@ def setContact(): #사용자 입력으로 주소록 받기함수
   phoneNumber = phoneNumber.strip()
   eMail = eMail.strip()
   addr = addr.strip()
-  print(f'"{name}", "{phoneNumber}", "{eMail}", "{addr}"')
+  #print(f'"{name}", "{phoneNumber}", "{eMail}", "{addr}"')
+  contact = Contact(name, phoneNumber, eMail, addr) # 매개변수명과 동일하게 로컬변수 이름 지정
+  return contact
+
+def clearConsole():
+  cmd = 'clear' #mac OS, Linux, Unix 명령어
+  if os.name in ('nt', 'dos'): # window
+    cmd = 'cls' # window 명령어
+  os.system(cmd) # 명령어 실행
 
 def displayMenu():
   menu = ('주소록 프로그램\n'
@@ -40,10 +50,29 @@ def displayMenu():
   sel = int(input('메뉴입력 : '))
   return sel
 
+def getContacts(lst): # 리스트를 받아서 출력
+  for item in lst:
+    print(item)
+
 def run():
+  #연락처를 담을 주소록 리스트 생성
+  lstContact = []
+
+  clearConsole() # 화면을 클리어
   while True:
     selMenu = displayMenu()
-    if selMenu == 4:
+    if selMenu == 1: # 연락처 추가
+      clearConsole()
+      contact = setContact()
+      lstContact.append(contact)
+      #print(lstContact)
+      input() #엔터 입력 유도
+      clearConsole()
+    elif selMenu == 2: # 연락처 출력
+      clearConsole()
+      getContacts(lstContact)
+      input(); clearConsole() 
+    elif selMenu == 4: # 프로그램 종료
       break
 
 if __name__ == '__main__': # 메인엔트리
