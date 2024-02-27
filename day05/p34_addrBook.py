29cf2418fc23f4e25f143e8d9d8429baf92f17c8
yoonji <dbswl341@naver.com>
2024-02-27 15:27:44 +0900

    0227

--- day05/p34_addrBook.py
+++ day05/p34_addrBook.py
@@ -52,9 +52,10 @@ def clearConsole():
   os.system(cmd) # 명령어 실행
 
 def delContact(lst, name): # 연락처 삭제함수
-  for i, item in enumerate(lst):
-    if item.isNameExist(name):
-      del lst[i]
+  for i in range(len(lst)-1, -1, -1): # 리스트를 내림차순하여 뒤에서부터 삭제
+     item = lst[i]
+     if item.isNameExist(name):
+        del lst[i]
 
 def saveContact(lst): # 연락처 저장함수
   with open('./contacts.txt', mode='w', encoding='utf-8') as fp:
@@ -63,14 +64,19 @@ def saveContact(lst): # 연락처 저장함수
       fp.write(f'{name}/{phoneNumber}/{eMail}/{addr}\n')
 
 def loadContact(lst): # 처음 실행시 데이터 읽어오는 함수
-  with open('./contacts.txt', mode='r', encoding='utf-8') as fp :
-    while True:
-      line = fp.readline()
-      if not line: break
+  try:
+    with open('./contacts.txt', mode='r', encoding='utf-8') as fp :
+      while True:
+        line = fp.readline()
+        if not line: break
 
-      lines = line.replace('\n','').split('/')
-      contact = Contact(name=lines[0], phoneNumber=lines[1], eMail=lines[2], addr=lines[3])
-      lst.append(contact)
+        lines = line.replace('\n','').split('/')
+        contact = Contact(name=lines[0], phoneNumber=lines[1], eMail=lines[2], addr=lines[3])
+        lst.append(contact)
+  except: # 연락처 파일이 없으면 새로 만들어줌
+    f = open('./contacts.txt',mode='w',encoding='utf-8')
+    f.close()
+
 
 
 def displayMenu():
@@ -80,7 +86,10 @@ def displayMenu():
           '3. 연락처 삭제\n'
           '4. 종료\n')
   print(menu)
-  sel = int(input('메뉴입력 : '))
+  try:
+    sel = int(input('메뉴입력 : '))
+  except : # 메뉴값을 1~4 이외의 문자를 입력할때 예외처리
+    sel = 0
   return sel
 
 def getContacts(lst): # 리스트를 받아서 출력
@@ -89,32 +98,40 @@ def getContacts(lst): # 리스트를 받아서 출력
     print(item)
 
 def run():
-  #연락처를 담을 주소록 리스트 생성
-  lstContact = []
-  loadContact(lstContact) # 연락처 로드
+    # 연락처를 담을 주소록 리스트 생성
+    lstContact = []
+    loadContact(lstContact)  # 연락처 로드
 
-  clearConsole() # 화면을 클리어
-  while True:
-    selMenu = displayMenu()
-    if selMenu == 1: # 연락처 추가
-      clearConsole()
-      contact = setContact()
-      lstContact.append(contact)
-      #print(lstContact)
-      input() #엔터 입력 유도
-      clearConsole()
-    elif selMenu == 2: # 연락처 출력
-      clearConsole()
-      getContacts(lstContact)
-      input(); clearConsole()
-    elif selMenu == 3: # 연락처 삭제
-      clearConsole()
-      name = input('삭제할 이름 입력 : ')
-      delContact(lstContact, name)
-      clearConsole()
-    elif selMenu == 4: # 프로그램 종료
-      saveContact(lstContact)
-      break
+    clearConsole()  # 화면을 클리어
+    while True:
+        selMenu = displayMenu()
+        if selMenu == 1:  # 연락처 추가
+            clearConsole()
+            try:
+                contact = setContact()
+                if contact is not None:
+                    lstContact.append(contact)
+                    input('입력 성공!')
+                else:
+                    input('입력 실패')  # 엔터 입력 유도
+            except Exception as e:  # 예외 처리
+                print(f'오류 발생: {e}')
+                input('입력 실패')  # 엔터 입력 유도
+
+            clearConsole()
+        elif selMenu == 2:  # 연락처 출력
+            clearConsole()
+            getContacts(lstContact)
+            input()
+            clearConsole()
+        elif selMenu == 3:  # 연락처 삭제
+            clearConsole()
+            name = input('삭제할 이름 입력 : ')
+            delContact(lstContact, name)
+            clearConsole()
+        elif selMenu == 4:  # 프로그램 종료
+            saveContact(lstContact)
+            break
 
 if __name__ == '__main__': # 메인엔트리
   print('프로그램 시작')
