ca4db0d20bdea6ef80bf265b7363634eb3eb4a62
yoonji <dbswl341@naver.com>
2024-02-28 16:59:07 +0900

    0228

--- day06/p39_naverNewsApp.py
+++ day06/p39_naverNewsApp.py
@@ -9,6 +9,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+
+import webbrowser
 from naverSearch import NaverSearch
 
 class qtApp(QWidget):
@@ -21,8 +23,18 @@ class qtApp(QWidget):
         self.setWindowIcon(QIcon('./images/news.png')) # 아이콘 파일에 맞춰서 변경
         # 버튼 시그널처리 
         self.btnSearch.clicked.connect(self.btnSearchClicked) # ui파일내 위젯은 자동완성X 
+        self.txtSearchWord.returnPressed.connect(self.btnSearchClicked) #검색버튼 시그널함수를 연결!
+        self.btnSearch.clicked.connect(self.btnSearchClicked) # ui파일 내 위젯은 자동완성 안됨
+        self.tblSearchResult.itemSelectionChanged.connect(self.tblResultSelected)
 
         self.show()
+        
+    def tblResultSelected(self):    # 테이블 클릭시 처리
+        select = self.tblSearchResult.currentRow()  # 현재 선택된 행위 인덱스
+        url = self.tblSearchResult.item(select, 1).text()
+        QMessageBox.about(self, 'popup', url)
+        webbrowser.open(url)
+
 
     def btnSearchClicked(self): # 검색 버튼 클릭시 처리
         searchWord = self.txtSearchWord.text().strip()
@@ -36,8 +48,25 @@ class qtApp(QWidget):
         # 검색시작
         api = NaverSearch() # api 객체생성
         jsonSearch = api.getSearchResult(searchWord)
-        print(jsonSearch)
+        #print(jsonSearch)
+        self.makeTable(jsonSearch) #검색결과로 리스트뷰를 만드는 함수 호출
 
+    def makeTable(self, data):
+        result = data['items'] #네이버 검색결과의 키 items의 값들만 활용
+        # tblSearchResult 리스트뷰 작업
+        self.tblSearchResult.setColumnCount(2)
+        self.tblSearchResult.setRowCount(len(result)) #10개면 10개
+        self.tblSearchResult.setHorizontalHeaderLabels(['기사제목','뉴스링크'])
+
+        n = 0
+        for post in result:
+            self.tblSearchResult.setItem(n, 0, QTableWidgetItem(post['title']))
+            self.tblSearchResult.setItem(n, 1, QTableWidgetItem(post['link']))
+            n += 1
+
+        self.tblSearchResult.setColumnWidth(0, 465)
+        self.tblSearchResult.setColumnWidth(1, 200)
+        self.tblSearchResult.setEditTriggers(QAbstractItemView.NoEditTriggers) # 컬럼 더블클릭 금지
 
     def closeEvent(self, QCloseEvent) -> None:
         re = QMessageBox.question(self, '종료확인', '종료하시겠습니까?', QMessageBox.Yes|QMessageBox.No)
